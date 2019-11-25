1f47bcdb63c352adb8b2dc88913164046cac169c
ddunmire <ddunmire@splunk.com>
2019-11-25 10:10:17 -0500

    removed junk file and added more environment  variables with description to trigger scripts

--- bin/pwnybrau_read_AtlasPH_stdout.sh
+++ bin/pwnybrau_read_AtlasPH_stdout.sh
@@ -7,9 +7,14 @@
 #    Sample output:  {"timestamp":"2019-11-18T01:49:10.927006+00:00", "PH":"3.768", "Device":"?I,pH,2.12"}
 #
 #
-export output="STDOUT"  # output destination: STDOUT, LOG or HEC
-export listentime=-1     # total time to take measurements (note -1 = infinity)
-export sleeptime=1      # time to wait between measurements
+export output="STDOUT"            # output destination: STDOUT, LOG or HEC
+#export logfile="pwnybrau_ph.log" # USED only with output=LOG
+export listentime=1               # total time to take measurements (note -1 = infinity)
+export sleeptime=1                # time to wait between measurements
+#export i2cbus=1                  # i2c bus number to use:  default = 1
+#export i2caddress=99             # i2c address for PH device:  default = 99
+
+# Note --show_device_info         # print atlas device info
 
 # execute sevice
 if [ -z ${SPLUNK_HOME} ]; then
--- bin/pwnybrau_read_AtlasTEMP_stdout.sh
+++ bin/pwnybrau_read_AtlasTEMP_stdout.sh
@@ -6,10 +6,15 @@
 #    OUTPUT: json :  timestamp,Temperature, unit
 #    Sample output:  {"timestamp":"2019-11-15T21:45:21.400057+00:00", "ChamberTemp":"78.919", "Unit":"f"}
 #
-export output="STDOUT"  # output destination: STDOUT, LOG or HEC
-export listentime=-1     # total time to take measurements (note -1 = infinity)
-export sleeptime=1      # time to wait between measurements
-export unit="f"         # temperature unit: c,f or k
+export output="STDOUT"            # output destination: STDOUT, LOG or HEC
+#export logfile="pwnybrau_ph.log" # USED only with output=LOG
+export listentime=1               # total time to take measurements (note -1 = infinity)
+export sleeptime=1                # time to wait between measurements
+export unit="f"                   # temperature unit: c,f or k
+#export i2cbus=1                  # i2c bus number to use:  default = 1
+#export i2caddress=102            # i2c address for PH device:  default = 102
+
+# Note --show_device_info         # print atlas device info
 
 # execute sevice
 if [ -z ${SPLUNK_HOME} ]; then
