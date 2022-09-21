8a1d0c4ebbc1fadf7c0d6e6342175bab2dc9ad39
Damon <ddunmire@splunk.com>
2022-09-21 11:18:11 -0400

    Reworked to use atlas-i2c==0.3.1 package.  Removed lots of redundancy

---
+++ examples/SplunkUF/bin/pwnybrau_read_AtlasPH_stdout.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# pwnybrau_read_AtlasPH_stdout.sh
+# Simple shortcut script to be called by a splunk univeral forwarder
+# Scripts calls python3 based script: AtlasPH.py
+#    OUTPUT: json :  timestamp,Temperature, unit
+#    Sample output:   {"timestamp":"2019-11-18T01:49:10.927006+00:00", "name": "AtlasPH", "measurement":"3.768"}
+#                     {"timestamp":"2019-11-18T01:49:10.927006+00:00", "name": "AtlasPH", "measurement":"3.768", "sensor":{"type":"pH", "firmware":2.12}}
+#
+export output="STDOUT"            # output destination: STDOUT, LOG or HEC
+#export logfile="pwnybrau_ph"     # USED only with output=LOG
+export listentime=1               # total time to take measurements (note -1 = infinity)
+export sleeptime=1                # time to wait between measurements
+#export i2cbus=1                  # i2c bus number to use:  default = 1
+#export i2caddress=99             # i2c address for PH device:  default = 99
+
+# Note --show_device_info         # print atlas device info
+
+# execute sevice
+if [ -z ${SPLUNK_HOME} ]; then
+   DIRECTORY=$(cd `dirname $0` && pwd)
+   echo $DIRECTORY
+   python3 $DIRECTORY/AtlasPH.py --output=${output} --listentime=${listentime} --sleeptime=${sleeptime}
+else
+   python3 $SPLUNK_HOME/etc/apps/pwnybrau_atlas/bin/AtlasPH.py --output=${output} --listentime=${listentime} --sleeptime=${sleeptime}
+fi
---
+++ examples/SplunkUF/bin/pwnybrau_read_AtlasTEMP_stdout.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# pwnybrau_read_AtlasTEMP_stdout.sh
+# Simple shortcut script to be called by a splunk univeral forwarder
+# Scripts calls python3 based script: AtlasTEMP.py
+#    OUTPUT: json :  timestamp,Temperature, unit
+#    Sample output:  {"timestamp":"2019-11-15T21:45:21.400057+00:00", "name":"AtlasRTD", "measurement":"78.919", "Unit":"f"}
+#                    {"timestamp":"2019-11-15T21:45:21.400057+00:00", "name":"AtlasRTD", "measurement":"78.919", "Unit":"f", "sensor":{"type":"pH", "firmware":2.12}}
+#
+export output="STDOUT"            # output destination: STDOUT, LOG or HEC
+#export logfile="pwnybrau_temp"   # USED only with output=LOG
+export listentime=1               # total time to take measurements (note -1 = infinity)
+export sleeptime=1                # time to wait between measurements
+export unit="f"                   # temperature unit: c,f or k
+#export i2cbus=1                  # i2c bus number to use:  default = 1
+#export i2caddress=102            # i2c address for PH device:  default = 102
+
+# Note --show_device_info         # print atlas device info
+
+# execute sevice
+if [ -z ${SPLUNK_HOME} ]; then
+   DIRECTORY=$(cd `dirname $0` && pwd)
+   echo $DIRECTORY
+   python3 $DIRECTORY/AtlasTEMP.py --output=${output} --listentime=${listentime} --sleeptime=${sleeptime} --unit=${unit}
+else
+   python3 $SPLUNK_HOME/etc/apps/pwnybrau_atlas/bin/AtlasTEMP.py --output=${output} --listentime=${listentime} --sleeptime=${sleeptime} --unit=${unit}
+fi
